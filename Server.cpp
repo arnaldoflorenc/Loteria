b0beb8c103cb91b90b641313ef9d9fabf10027a9
arnaldoflorenc <arnaldoflorencio05@gmail.com>
2025-08-22 17:46:18 -0300

    Tentativa de escutar cliente

--- Server.cpp
+++ Server.cpp
@@ -4,14 +4,23 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <list>
-#include <string>
+#include <thread>
 
 list<int> aposta;
 
 using namespace std;
 
-void escutar(){
-
+void escutar(int clientSocket){
+    int apost_client;
+    while(true){
+        ssize_t tam = read(clientSocket, &apost_client, sizeof(apost_client));
+        if(tam <= 0){
+            close(clientSocket);
+            return;
+        }
+        aposta.push_back(apost_client);
+        cout <<"Aposta computada!!"<<aposta<<endl;
+    }
 }
 
 void sorteio(list<int> sorteada){
@@ -22,19 +31,17 @@ int main(){
     list<int> sorteada;
 
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-
     sockaddr_in serverAdress;
     serverAdress.sin_family = AF_INET;
     serverAdress.sin_port = htons(8080);
     serverAdress.sin_addr.s_addr = INADDR_ANY;
-
     bind(serverSocket, (struct sockaddr*)&serverAdress, sizeof(serverAdress));
     listen(serverSocket,5);
 
-    int clientSocket = accept(serverSocket, nullptr, nullptr);
-    char buffer[1024] = {0};
-    recv(clientSocket, buffer, sizeof(buffer), 0);
-    cout<<"MSG cliente: "<<buffer<<endl;
+    while(true){
+        int clientSocket = accept(serverSocket, nullptr, nullptr);
+        thread(escutar,clientSocket).detach();
+    }
 
     close(serverSocket);
 
