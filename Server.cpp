f72e49d5e8422d0f6f1a1f8940d51c3f5b4be72e
luigishimabukuro <luigi.p.shima@gmail.com>
2025-08-30 19:54:46 -0300

    fase 1 praticamente finalizada, falta polimento

--- Server.cpp
+++ Server.cpp
@@ -2,42 +2,126 @@
 #include <iostream>
 #include <netinet/in.h>
 #include <sys/socket.h>
+#include <sstream>
 #include <unistd.h>
+#include <ctime>
+#include <chrono>
 #include <list>
-#include <string>
 #include <thread>
+#include <cstdlib>
 
 using namespace std;
 
-using std::list;
+enum class tipo_aposta{ //Enum que fala qual o tipo de aposta, se n houver especificando inicio e fim é padrão
+    PADRAO,
+    SELECIONADA
+};
 
 list<int> aposta;
 
-void escutar(){
-
+tipo_aposta defTipo(){
+    if(aposta.size() == 6){
+        return tipo_aposta::PADRAO;
+    } else{
+        return tipo_aposta::SELECIONADA;
+    }
 }
 
-void sorteio(list<int> sorteada){
-
+void registraAposta(string& msg){
+    stringstream msg_client(msg);    
+    int valor;
+    while(msg_client >> valor){
+        aposta.push_back(valor);
+    }
+    cout << "Aposta registrada! -> \t" + msg <<endl;
 }
 
-int main(){
-    list<int> sorteada;
+void escutar(int clientSocket){
+    char buffer[1024];
+    while(true){
+        ssize_t tam = read(clientSocket, buffer, sizeof(buffer)-1);
+        if(tam <= 0){
+            close(clientSocket);
+            return;
+        }
+        buffer[tam] = '\0';
+        string msg(buffer);
 
+        registraAposta(msg);
+
+        cout << "Aposta Recebida!"<<endl;
+        cout <<"Aposta computada!!\n"<<endl;
+        if (buffer[tam] == '\0') {
+            return;
+        }
+    }
+}
+
+void sorteio(int clientSocket){
+
+    while (aposta.size() < 6) {
+        this_thread::sleep_for(chrono::milliseconds(100));
+    }
+
+    this_thread::sleep_for(chrono::seconds(5));
+    
+    list<int> num_sorteados;
+    if(defTipo() == tipo_aposta::PADRAO){
+        for(int i=0; i<6; i++){//caso padrão sorteia de 0 a 60
+            num_sorteados.push_back(rand()%100);    
+        }
+    }else{
+        int minimo = aposta.front();
+        int max = aposta.front();
+        aposta.pop_front();
+        aposta.pop_front();
+        
+        for(int i=0; i<6; i++){//caso selecionado sorteia de minimo até max
+            num_sorteados.push_back(minimo + rand()%max);    
+        }
+    }
+    string result = (aposta == num_sorteados) ? "Você ganhou!!" : "Você perdeu... tente novamente";
+    stringstream aux;
+    for(int n : num_sorteados){
+        aux << n <<" ";
+    }
+    result += "\nNúmeros Sorteados: " + aux.str();
+    send(clientSocket, result.c_str(), result.size(), 0);
+    cout << "Resultado enviado ao cliente!" << endl;
+}
+
+int main(){ 
+    //hora em sec para seed
+    time_t now = time(nullptr);
+    tm* hr_local = localtime(&now);
+    int seg_seed = (hr_local->tm_hour * 3600) + (hr_local->tm_min * 60);
+    srand(seg_seed);
+
+    //inicio conectividade do server
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-
     sockaddr_in serverAdress;
     serverAdress.sin_family = AF_INET;
     serverAdress.sin_port = htons(8080);
     serverAdress.sin_addr.s_addr = INADDR_ANY;
-
     bind(serverSocket, (struct sockaddr*)&serverAdress, sizeof(serverAdress));
     listen(serverSocket,5);
-
+    cout<<"Servidor pronto para receber conexões\n";
     int clientSocket = accept(serverSocket, nullptr, nullptr);
-    char buffer[1024] = {0};
-    recv(clientSocket, buffer, sizeof(buffer), 0);
-    cout<<"MSG cliente: "<<buffer<<endl;
+    cout<<"Client conectado\n";
+    while(true){
+        
+
+        thread t1(escutar,clientSocket);//escutar novos clientes
+        thread t2(sorteio, clientSocket);//sortear nums
+        t1.join();
+        t2.join();
+
+        aposta.clear();
+        cout<<"Aposta finalizada, esperando novo cliente\n";
+    }
+    
+    cout << "Servidor encerrando..." << endl;
+
 
     close(serverSocket);
 
--- cliente.cpp
+++ cliente.cpp
@@ -4,14 +4,21 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <thread>
+#include <chrono>
+#include <ctime>
+#include <iomanip>
 
 using namespace std;
 
 void inputcliente (int clientSocket){
     string msg;
+    cout << "Digite sua aposta (6 números separados por espaço):" << endl;
     while (true){    
     getline(cin, msg);
+    if (msg == "exit") break;
+    if (!msg.empty()){
     send (clientSocket, msg.c_str(), msg.size(), 0);
+    }
     }
 }
 
@@ -22,12 +29,14 @@ void recebecliente (int clientSocket){
     cout<<"Mensagem do servidor: "<<buffer<<endl;
     memset(buffer, 0, sizeof(buffer));
     }
-
 }
 
 int main(){
-
+    auto now = chrono::system_clock::now();
+    time_t tt = chrono::system_clock::to_time_t(now);
     int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    tm* now_tm = localtime(&tt);
+    string msg;
 
     sockaddr_in serverAdress;
     serverAdress.sin_family = AF_INET;
@@ -35,16 +44,13 @@ int main(){
     serverAdress.sin_addr.s_addr = INADDR_ANY;
 
     connect(clientSocket, (struct sockaddr*)&serverAdress, sizeof(serverAdress));
-    
-    const char* message = "Ola do cliente!";
-    send(clientSocket, message, strlen(message),0);
-
+    cout<< put_time(now_tm, "%H:%M:%S") << ": CONECTADO!!"<< endl;
+    while(true){
     thread t1(inputcliente, clientSocket);
     thread t2(recebecliente, clientSocket);
-
     t1.join();
     t2.join();
-
+    }
 
     close(clientSocket);
     return 0;
